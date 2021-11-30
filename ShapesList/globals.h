e10a02176f749f90aa9cf9c86aec55a5538c9ba6
Floria Fang Zhang <74149949+floriafz23@users.noreply.github.com>
2021-11-30 14:48:58 -0500

    Create globals.h

---
+++ ShapesList/globals.h
@@ -0,0 +1,22 @@
+//
+// File globals.h provided by Professor Tarek Abdelrahman of University of Toronto
+//
+
+
+#ifndef globals_h
+#define globals_h
+
+
+#include <iostream>
+using namespace std;
+#include<string>
+
+// These arrays should make it easier to check that a name is not a reserved word
+// and that an entered shape type is valid
+#define NUM_KEYWORDS 6
+string keyWordsList[NUM_KEYWORDS]={"shape", "group", "move", "delete", "draw", "pool"};
+
+#define NUM_TYPES 3
+string shapeTypesList[NUM_TYPES]={"ellipse", "rectangle","triangle"};
+#endif /* globals_h */
+
