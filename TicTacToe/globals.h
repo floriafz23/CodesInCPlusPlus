ee8385b922c7ab6578aa64bc60df1c4fa42dcbe9
Floria Fang Zhang <74149949+floriafz23@users.noreply.github.com>
2021-11-30 14:38:57 -0500

    Create globals.h

---
+++ TicTacToe/globals.h
@@ -0,0 +1,21 @@
+//
+//  globals.h provided by Professor Tarek Abdelrahman
+//
+
+#ifndef globals_h
+#define globals_h
+
+// Some basic definitions for the game
+#define X     1
+#define O    -1
+#define Empty 0
+
+#define boardSize 3
+
+// Forward declaration of the class GameState (defined in GameState.h)
+class GameState;
+
+// The prototype of the playMove function
+void playMove(GameState&);
+
+#endif /* globals_h */
